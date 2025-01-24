395f838cfbed146ae5f8ee613b5ff671b6bde8c9
Viacheslav Levkoniuk <viacheslav.levkoniuk@angloamerican.com>
2025-01-24 15:52:23 +0200

    added pretty-format output

--- cucumber.js
+++ cucumber.js
@@ -3,7 +3,7 @@ let common = [
     'Tests/Features/**/*.feature',                // Specify our feature files
     '--require-module ts-node/register',    // Load TypeScript module
     '--require Tests/Step-definitions/**/*.ts',   // Load step definitions
-    '--format progress-bar',                // Load custom formatter
+    '--format @cucumber/pretty-formatter',                // Load custom formatter
     '--format json:./Reports/cucumber_report.json'
   ].join(' ');
   
